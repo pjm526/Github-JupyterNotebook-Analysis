eef64e011f817ce6860816edfade6364c0845189
pjm526 <43018908+pjm526@users.noreply.github.com>
2020-02-14 16:40:25 -0500

    Add files via upload

--- Spark/spark_processing.py
+++ Spark/spark_processing.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-#import postgres
 import sys
 import pyspark
 from pyspark import SQLContext
@@ -9,64 +7,66 @@ from pyspark.sql import Row
 from pyspark.sql.types import StructType
 from pyspark.sql.types import StructField
 from pyspark.sql.types import StringType
+import pyspark.sql.types as T
 from pyspark.sql.functions import udf, expr, concat, col
 from notebook_processing import ProcessNotebooks
 
 
-class spark_processing(object):
 
-    def __init__(self):
-        self.spark = SparkSession \
-            .builder \
-            .appName("LibraryInsights") \
-            .getOrCreate()
+class spark_processing(object):
+	
+	
+	def __init__(self):
+		self.spark = SparkSession \
+		    .builder \
+		    .appName("LibraryInsights") \
+		    .getOrCreate()
+
+		self.spark.sparkContext.addPyFile('notebook_processing.py')
+		self.spark.sparkContext.addPyFile('timestamp.py')
+		self.spark.sparkContext.addPyFile('import_lib.py')
+		
+
+	def nb_repo_df(self):
+		
+		"""
+		Extract notebook ids and repository ids for each Jupyter Notebook
+
+		"""
+		
+		df = self.spark.read.format("csv").option("header", "true").load("s3a://summarydatacsv/csv/notebooks.csv")
+		df_selected_columns = df.select(df['nb_id'], df['repo_id'])
+	
+		return df_selected_columns
+
+
+	def NotebookMapper(self,df_selected_columns):
+		
+		"""
+		Distribute notebook ids to Spark workers
+		Used flatmap to aggregrate users for each month for each library
+
+		"""
+		process_notebooks = ProcessNotebooks()
+
+		lib_time_rdd = df_selected_columns.rdd.flatMap(process_notebooks.process_each_notebook) \
+							.filter(lambda x: x[0][0] != 'nolibrary') \
+							.reduceByKey(lambda n,m: n+m) \
+							.map(lambda x: (x[0][0],x[0][1],x[1]))
 
 	
-		#sqlContext = SQLContext(sc)
+		df_schema = StructType([StructField("library", StringType(), False),
+						 StructField("datetime", StringType(), False ),
+						 StructField("lib_counts", StringType(), False )])
+	
+		final_df = (
+		    lib_time_rdd \
+		    .map(lambda x: [x[0],x[1],x[2]]) \
+		    .toDF(df_schema) \
+		    .select("library","datetime","lib_counts")
+		)
 
-        # Add modules
-        self.spark.sparkContext.addPyFile('notebook_processing.py')
-        self.spark.sparkContext.addPyFile('timestamp.py')
-        self.spark.sparkContext.addPyFile('import_lib.py')
-
-        self.bucket = "gotcha"
-
-
-    def nb_repo_df(self):
-	print("inside the nb_repo_df function")
-	df = self.spark.read.format("csv").option("header", "true").load("s3a://gotcha/sample_data/data/csv/notebooks_sample.csv")
-	df_selected_columns = df.select(df['nb_id'], df['repo_id'])
-	print(df_selected_columns.show(10))
-	return df_selected_columns
+		
+		return final_df
 
 
-    def NotebookMapper(self,df_selected_columns):
-
-        print('got file df ..................................')
-
-        # Farm out jupyter notebook files to Spark workers with a flatMap and
-        # aggregrate users for each month for each library
-        process_notebooks = ProcessNotebooks()
-        processed_rdd = df_selected_columns.rdd.flatMap(process_notebooks.process_each_notebook) \
-                        .filter(lambda x: x[0][0] != 'nolibrary') \
-                        .reduceByKey(lambda n,m: n+m) \
-                        .map(lambda x: (x[0][0],x[0][1],x[1]))
-        print('got processed rdd ..................................')
-
-		#print(processed_rdd.collect())
-        # Save processed rdd as a dataframe using the following schema:
-        processed_schema = StructType([StructField("library", StringType(), False),
-                                         StructField("datetime", StringType(), False ),
-                                         StructField("lib_counts", StringType(), False )])
-		#print("Schema processed")
-        processed_df = (
-            processed_rdd \
-            .map(lambda x: [x[0],x[1],x[2]]) \
-            .toDF(processed_schema) \
-            .select("library","datetime","lib_counts")
-        )
-		#print(processed_df.show(5))
-        return processed_df
-
-
-    
